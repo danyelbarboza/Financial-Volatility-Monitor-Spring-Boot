f1da56cf364042894b3767b92051b9208274a4f8
Danyel Barboza <dhgbarboza@gmail.com>
2025-06-11 14:03:37 -0300

    Implementação da Entity

---
+++ src/main/java/com/danyelbarboza/volatility_monitor/entity/FinancialData.java
@@ -0,0 +1,34 @@
+package com.danyelbarboza.volatility_monitor.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "financial_asset_volatility")
+public class FinancialData {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "ticker")
+    private String ticker;
+
+    @Column(name = "close_price")
+    private Double closePrice;
+
+    @Column(name = "atr_14")
+    private Double atr14;
+
+    @Column(name = "record_timestamp")
+    private Timestamp recordTimestamp;
+}
